d0ac50ae3cf8e6f3724713b62f1500aad93df756
Kingsley Segu Acquah <75037978+Acquah-Kingsley-Segu@users.noreply.github.com>
2023-11-08 02:48:07 -0800

    Signup page down, login page to signup page  done, and signup page to login page done (#2)

--- Frontend/LearnRight/src/Components/LogIn.jsx
+++ Frontend/LearnRight/src/Components/LogIn.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { MdClose, MdOutlineLock, MdOutlineMail } from "react-icons/md";
 import ReactModal from "react-modal";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [openModal, setOpenModal] = useState(true)
+  const [openModal, setOpenModal] = useState(true);
   const navigate = useNavigate();
-  
-  return(
+
+  return (
     <ReactModal
       isOpen={openModal}
       className="bg-white flex flex-col w-1/4 border-0 border-white rounded-lg shadow-md p-2 h-3/5 overflow-y-auto scroll scroll-smooth"
@@ -18,7 +18,7 @@ const Login = () => {
         <button
           onClick={() => {
             setOpenModal(false);
-            navigate('..');
+            navigate("..");
           }}
         >
           <MdClose className="text-2xl text-red-500 hover:bg-red-500 hover:text-white rounded-sm" />
@@ -26,36 +26,57 @@ const Login = () => {
       </div>
       <form action="" method="post" className="flex-1 flex flex-col">
         <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[30px] mx-3 h-[40px] flex justify-center">
-          <MdOutlineMail className="h-full text-[#333] text-2xl"/>
-          <input className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out" type="email" placeholder="Enter your email" required />
-          {/* <i className="uil uil-envelope-alt email"></i> have to use react icon here */} 
+          <MdOutlineMail className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
+          {/* <i className="uil uil-envelope-alt email"></i> have to use react icon here */}
         </div>
         <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[15px] mx-3 h-[40px] flex justify-center">
-          <MdOutlineLock className="h-full text-[#333] text-2xl"/>
-          <input className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out" type="password" placeholder="Enter your password" required />
+          <MdOutlineLock className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="password"
+            placeholder="Enter your password"
+            required
+          />
           {/* <i className="uil uil-lock password"></i>
           <i className="uil uil-eye-slash pw_hide"></i> will have to use react icons here */}
         </div>
         <div className="mt-[14px] bg-greeen-200 mx-3 flex items-center justify-between">
           <span className="flex gap-x-[8px] whitespace-nowrap">
             <input className="accent-[#7d2ae8]" type="checkbox" id="check" />
-            <label className="text-[12px] cursor-pointer select-none text-[#0b0217]" for="check">Remember me</label>
+            <label
+              className="text-[12px] cursor-pointer select-none text-[#0b0217]"
+              for="check"
+            >
+              Remember me
+            </label>
           </span>
-          <a href="#" className="forgot_pw text-violet-400 text-sm">Forgot password?</a>
+          <a href="#" className="forgot_pw text-violet-400 text-sm">
+            Forgot password?
+          </a>
         </div>
-  
-        <button className="py-[6px] px-[24px] mx-3 mt-10 hover:bg-white hover:border-[1.5px] hover:border-violet-700 hover:text-black text-white bg-violet-700 border-[1.5px] border-solid border-[#fff] bg-transparent rounded-[6px] cursor-pointer">Login Now</button>
-  
+
+        <button className="py-[6px] px-[24px] mx-3 mt-10 hover:bg-white hover:border-[1.5px] hover:border-violet-700 hover:text-black text-white bg-violet-700 border-[1.5px] border-solid border-[#fff] bg-transparent rounded-[6px] cursor-pointer">
+          Login Now
+        </button>
+
         <div className="text-xs text-center mt-[15px]">
-          Don't have an account? <a href="#" id="signup" className="text-violet-400">Signup</a>
+          Don't have an account?{" "}
+          <NavLink to={"/signup"} id="signup" className="text-violet-400">
+            Signup
+          </NavLink>
         </div>
       </form>
     </ReactModal>
-  )
-}
+  );
+};
 
 export default Login;
-
 
 /**
  * <ReactModal 
--- Frontend/LearnRight/src/Components/SignUp.jsx
+++ Frontend/LearnRight/src/Components/SignUp.jsx
@@ -1,5 +1,117 @@
+import { useState } from "react";
+import {
+  MdClose,
+  MdOutlineLock,
+  MdOutlineMail,
+  MdOutlinePersonPinCircle,
+} from "react-icons/md";
+import ReactModal from "react-modal";
+import { NavLink, useNavigate } from "react-router-dom";
+
 const Signup = () => {
-  return <h1>SignUp Page</h1>
-}
+  const [openModal, setOpenModal] = useState(true);
+  const navigate = useNavigate();
+
+  return (
+    <ReactModal
+      isOpen={openModal}
+      className="bg-white flex flex-col w-1/4 border-0 border-white rounded-lg shadow-md p-2 h-3/5 overflow-y-auto scroll scroll-smooth"
+      overlayClassName="bg-black/10 fixed inset-0 flex justify-center items-center h-full"
+    >
+      <div className="flex items-center px-2 py-2">
+        <h2 className="flex-1 text-center font-medium text-xl">Signup</h2>
+        <button
+          onClick={() => {
+            setOpenModal(false);
+            navigate("..");
+          }}
+        >
+          <MdClose className="text-2xl text-red-500 hover:bg-red-500 hover:text-white rounded-sm" />
+        </button>
+      </div>
+      <form action="" method="post" className="flex-1 flex flex-col">
+        <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[10px] mx-3 h-[40px] flex justify-center">
+          <MdOutlinePersonPinCircle className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="text"
+            placeholder="Create a username"
+            required
+          />
+        </div>
+        <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[10px] mx-3 h-[40px] flex justify-center">
+          <MdOutlineMail className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[10px] mx-3 h-[40px] flex justify-center">
+          <MdOutlineLock className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="password"
+            placeholder="Create a password"
+            required
+          />
+        </div>
+        <div className="border-b-[1.5px] border-solid border-[#aaaaaa] relative mt-[10px] mx-3 h-[40px] flex justify-center">
+          <MdOutlineLock className="h-full text-[#333] text-2xl" />
+          <input
+            className="w-full h-full outline-none py-0 pl-1 text-[#333] text-sm transition-all duration-[200ms] ease-in-out"
+            type="password"
+            placeholder="Confirm password"
+            required
+          />
+        </div>
+
+        <button className="py-[6px] px-[24px] mx-3 mt-5 hover:bg-white hover:border-[1.5px] hover:border-violet-700 hover:text-black text-white bg-violet-700 border-[1.5px] border-solid border-[#fff] bg-transparent rounded-[6px] cursor-pointer">
+          Signup Now
+        </button>
+
+        <div className="text-xs text-center mt-[15px]">
+          Already have an account?
+          <NavLink to={"/login"} id="signup" className="text-violet-400">
+            Login
+          </NavLink>
+        </div>
+      </form>
+    </ReactModal>
+  );
+};
 
 export default Signup;
+
+{
+  /**
+<div class="form signup_form">
+      <form action="#">
+        <h2>Signup</h2>
+        <div class="input_box">
+          <input type="email" placeholder="Enter your email" required />
+          <i class="uil uil-envelope-alt email"></i>
+        </div>
+        <div class="input_box">
+          <input type="password" placeholder="Create password" required />
+          <i class="uil uil-lock password"></i>
+          <i class="uil uil-eye-slash pw_hide"></i>
+        </div>
+        <div class="input_box">
+          <input type="password" placeholder="Confirm password" required />
+          <i class="uil uil-lock password"></i>
+          <i class="uil uil-eye-slash pw_hide"></i>
+        </div>
+        <button class="button">Signup Now</button>
+        <div class="login_signup">
+          Already have an account?{" "}
+          <a href="#" id="login">
+            Login
+          </a>
+        </div>
+      </form>
+    </div>
+
+*/
+}
