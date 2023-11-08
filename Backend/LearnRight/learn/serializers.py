4e0b9730923a3ad46c436b29da76f733ea31b6ba
Kingsley Segu Acquah <75037978+Acquah-Kingsley-Segu@users.noreply.github.com>
2023-11-08 08:12:36 -0800

    LearnRight Serializers defined (#3)

---
+++ Backend/LearnRight/learn/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import *
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+class ConceptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Concept
+        fields = '__all__'
+
+class SubConceptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubConcept
+        fields = '__all__'
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+class ReviewQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewQuestion
+        fields = '__all__'
