e404340541618db62d2adab29b8e65559387d261
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-08 23:15:12 +0100

    CRUD for the various entities are defined

--- Backend/LearnRight/learn/urls.py
+++ Backend/LearnRight/learn/urls.py
@@ -1,1 +1,17 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('subject', SubjectListCreateView.as_view()),
+    path('subject/<int:pk>', SubjectRetrieveUpdateDestroyView.as_view()),
+    path('topic', TopicListCreateView.as_view()),
+    path('topic/<int:pk>', TopicRetrieveUpdateDestroyView.as_view()),
+    path('concept', ConceptListCreateView.as_view()),
+    path('concept/<int:pk>', ConceptRetrieveUpdateDestroyView.as_view()),
+    path('subconcept', SubConceptListCreateView.as_view()),
+    path('subconcept/<int:pk>', SubConceptRetrieveUpdateDestroyView.as_view()),
+    path('note', NoteListCreateView.as_view()),
+    path('note/<int:pk>', NoteRetrieveUpdateDestroyView.as_view()),
+    path('review_questions', ReviewQuestionListCreateView.as_view()),
+    path('review_questions/<int:pk>', ReviewQuestionRetrieveUpdateDestroyView.as_view()),
+]
