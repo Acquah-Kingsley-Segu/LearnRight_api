61a2d5c9f0f13fdd1b4a68d6ad2b407e1be5da56
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-08 17:01:37 +0100

    LearnRight models defined

--- Backend/LearnRight/config/settings.py
+++ Backend/LearnRight/config/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'learn'
+    'learn',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Backend/LearnRight/learn/models.py
+++ Backend/LearnRight/learn/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Concept(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+class SubConcept(models.Model):
+    name = models.CharField(max_length=255)
+    concept = models.ForeignKey(Concept, on_delete=models.CASCADE)
+
+class Note(models.Model):
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    conceot = models.ForeignKey(Concept, on_delete=models.CASCADE)
+
+class ReviewQuestion(models.Model):
+    question = models.CharField(max_length=255)
