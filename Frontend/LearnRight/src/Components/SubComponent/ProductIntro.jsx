ea45071fd4152db512f2b365d7bc7fa11b2f110b
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-08 11:44:06 +0100

    Signup page down, login page to signup page  done, and signup page to login page done

--- Frontend/LearnRight/src/Components/SubComponent/ProductIntro.jsx
+++ Frontend/LearnRight/src/Components/SubComponent/ProductIntro.jsx
@@ -1,19 +1,24 @@
 import { NavLink } from "react-router-dom";
 
 const ProductInfo = () => {
-  return(
+  return (
     <aside className="w-1/3 relative top-32 left-20 p-3 text-white">
       <article className="mb-5">
         <h1 className="text-4xl mb-3">Lorem Ipsum</h1>
         <p>
-          "Unlock Your Full Learning Potential with LearnRight:
-          Where Proven Techniques Meet Personalized Learning Paths. 
-          Join Us Today and Elevate Your Educational Journey!"
+          "Unlock Your Full Learning Potential with LearnRight: Where Proven
+          Techniques Meet Personalized Learning Paths. Join Us Today and Elevate
+          Your Educational Journey!"
         </p>
       </article>
-      <NavLink className="bg-blue-600 p-3 text-white font-medium rounded-lg my-2 text-lg">Sign up now</NavLink>
+      <NavLink
+        to={"/signup"}
+        className="bg-blue-600 p-3 text-white font-medium rounded-lg my-2 text-lg"
+      >
+        Sign up now
+      </NavLink>
     </aside>
-  )
-}
+  );
+};
 
 export default ProductInfo;
