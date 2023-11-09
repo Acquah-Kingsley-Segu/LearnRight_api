64b999296ed1a64b6f0aa99ce5f8dd7dad1b02c3
MaxFine <gibboel5@gmail.com>
2023-11-09 08:27:16 +0000

    logout configs

--- Backend/LearnRight/accounts/urls.py
+++ Backend/LearnRight/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login', AccountAuthToken.as_view()),
     path('signup', SignUpView.as_view()),
     path('users', UsersListView.as_view()),
+    path('logout', Logout.as_view(), name='logout'),
     #path('api/logout/', views.logout, name='api_logout'),  # Logout view
     #path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- Backend/LearnRight/config/settings.py
+++ Backend/LearnRight/config/settings.py
@@ -52,9 +52,9 @@ REST_FRAMEWORK = {
         #"rest_framework.authentication.SessionAuthentication",
         'rest_framework.authentication.TokenAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 MIDDLEWARE = [
